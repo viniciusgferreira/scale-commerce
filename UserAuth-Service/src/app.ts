7bd505a7c4363882fdefec6c5129729ba87c27c5
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-12-05 18:38:50 -0300

    mongoose connection to mongodb in docker

--- UserAuth-Service/src/app.ts
+++ UserAuth-Service/src/app.ts
@@ -1,13 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 // 404 - API NOT FOUND
 app.use('/', (req, res) => {
-  res.status(404).send('API UserAuth - Endpoint not found');
+  res.status(404).send('API UserAuth - Endpoint not found!');
 });
 
-app.listen(port, () => {
-  console.log(`API UserAuth Service is running on port http://localhost:${port}`);
-});
+//DB CREDENTIALS
+const dbUser = process.env.DBUSER;
+const dbPass = process.env.DBPASS;
+
+// MONGODB CONNECTION
+const MONGO_NAME_STR = "mongodb://mongo:27017";
+
+mongoose.connect(MONGO_NAME_STR)
+  .then(() => {
+    console.log('Conectado ao MongoDB');
+  })
+  .catch((err) => console.log(err));
+
+app.listen(port, () => console.log(`API UserAuth Service is running on port http://localhost:${port}`));
+
