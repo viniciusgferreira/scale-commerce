954dfaf12985c8ba682d410422a0d538b533f6e8
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-12-05 20:01:49 -0300

    only start server with mongodb connected

--- UserAuth-Service/src/app.ts
+++ UserAuth-Service/src/app.ts
@@ -1,9 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
-const app = express();
-const port = process.env.PORT || 3001;
-
+import { router } from './routes/router';
 
 //DB CREDENTIALS
 const dbUser = process.env.DBUSER;
@@ -15,12 +12,14 @@ const MONGO_NAME_STR = "mongodb://localhost:27017";
 mongoose.connect(MONGO_NAME_STR)
   .then(() => {
     console.log('Conectado ao MongoDB');
+    // Create express server
+    const app = express();
+    const port = process.env.PORT || 3001;
     app.listen(port, () => console.log(`API UserAuth Service is running on port http://localhost:${port}`));
+
+    // 404 - API NOT FOUND
+    app.use('/', (req, res) => {
+      res.status(404).send('API UserAuth - Endpoint not found!');
+    });
   })
   .catch((err) => console.log(err));
-
-
-// 404 - API NOT FOUND
-app.use('/', (req, res) => {
-  res.status(404).send('API UserAuth - Endpoint not found!');
-});
