a9cbe351a3fa67329e0ce36e5d13a6781436483c
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-12-06 06:58:32 -0300

    User Schema and model

---
+++ UserAuth-Service/src/models/User.ts
@@ -0,0 +1,12 @@
+import { model, Schema } from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+
+const userSchema = new Schema({
+  username: { type: String, lowercase: true, unique: true, required: [true, 'cannot be blank'], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },
+  hash: String,
+}, { timestamps: true });
+
+// VALIDATE UNIQUE USERNAME
+userSchema.plugin(uniqueValidator, { message: 'username already taken.' });
+
+export const User = model('User', userSchema)
