461f5c28d01ba682ac2e21d6e2e7357ea2f5584b
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-12-06 18:41:52 -0300

    fix: redirection path

--- API-Gateway/src/server.ts
+++ API-Gateway/src/server.ts
@@ -10,24 +10,30 @@ const port = process.env.PORT || 3000;
 const userAuthServicePort = 3001;
 const ordersServicePort = 3002;
 
+
 // Test Request Uri
 function redirectToService(req: Request) {
   console.log('New request at ' + req.path);
-  if (req.path.startsWith('/userauth')) return `http://localhost:${userAuthServicePort}/api/${req.path}`;
-  else if (req.path.startsWith('/orders')) return `http://localhost:${ordersServicePort}/api/${req.path}`;
+  if (req.path.startsWith('/userauth')) {
+    return `http://localhost:${userAuthServicePort}/api${req.path}`;
+  }
+  else if (req.path.startsWith('/orders')) {
+    return `http://localhost:${ordersServicePort}/api/${req.path}`;
+  }
   return 'http://localhost:3000/';
 }
 
 
 
 //REDIRECTION
-app.use('/api/', (req, res, next) => {
+app.use('/api', (req, res, next) => {
   httpProxy(redirectToService(req))(req, res, next);
+  httpProxy(`http://localhost:${userAuthServicePort}/api/${req.path}`)(req, res, next);
 });
 
 // 404 - API NOT FOUND
 app.use('/', (req, res) => {
-  res.status(404).send('API Endpoint not found');
+  res.status(404).send('API Gateway Endpoint not found');
 });
 
 app.listen(port, () => {
