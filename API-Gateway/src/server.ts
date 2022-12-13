fb058db4a5ae47f3e2d1ab3cc7499eccd0901550
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-12-13 07:41:09 -0300

    refactor: redirection and bad request handle

--- API-Gateway/src/server.ts
+++ API-Gateway/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request } from 'express';
+import express, { Request, Response } from 'express';
 import httpProxy from 'express-http-proxy';
 
 
@@ -7,27 +7,32 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // SERVICES PORTS
-const userAuthServicePort = 3001;
+const userServicePort = 3001;
 const ordersServicePort = 3002;
-
 
 // Test Request Uri
 function redirectToService(req: Request) {
   console.log('New request at ' + req.path);
-  if (req.path.startsWith('/user-service')) {
-    return `http://localhost:${userAuthServicePort}/api${req.path}`;
+  const serviceReq = req.path.split('/');
+
+  switch (serviceReq[1]) {
+    case 'user-service':
+      return `http://localhost:${userServicePort}/api${req.path}`;
+      break;
+    case 'order-service':
+      return `http://localhost:${ordersServicePort}/api/${req.path}`;
+      break;
+    default:
+      console.log('Service URI not found');
+      return null;
+      break;
   }
-  else if (req.path.startsWith('/order-service')) {
-    return `http://localhost:${ordersServicePort}/api/${req.path}`;
-  }
-  return 'http://localhost:3000/';
 }
-
-
 
 //REDIRECTION
 app.use('/api', (req, res, next) => {
-  httpProxy(redirectToService(req))(req, res, next);
+  const uriCall = redirectToService(req);
+  !uriCall ? res.status(404).json({ message: 'Service not found' }) : httpProxy(uriCall)(req, res, next);
 });
 
 app.listen(port, () => {
