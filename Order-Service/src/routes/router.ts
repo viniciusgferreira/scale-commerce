54b326d944904f078d18777101472f429bc79d39
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-12-16 07:22:06 -0300

    edit and delete route

--- Order-Service/src/routes/router.ts
+++ Order-Service/src/routes/router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addOrder, listOrderById, listOrders } from "../controllers/order-controller";
+import { addOrder, editOrder, listOrderById, listOrders, removeOrder } from "../controllers/order-controller";
 
 export const router = Router();
 
@@ -8,14 +8,12 @@ router.get('/orders', listOrders);
 // GET USER INFO by ID
 router.get('/orders/:id', listOrderById);
 
-// GET USERS
-//router.get('/users', listUsers);
 
-// ADD USER
+// ADD ORDER
 router.post('/orders', addOrder);
 
-// EDIT USER INFO
-//router.put('/users', editUser);
+// EDIT ORDER INFO
+router.put('/orders', editOrder);
 
 // DELETE USER
-//router.delete('/users/:id', removeUser); * /
+router.delete('/orders/:id', removeOrder);
