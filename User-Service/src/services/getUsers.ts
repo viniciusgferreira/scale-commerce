ac7e195043058f028df9b7834194bc00f0cc47c8
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-12-19 19:33:25 -0300

    consuming events on listUsers request

--- User-Service/src/services/getUsers.ts
+++ User-Service/src/services/getUsers.ts
@@ -1,7 +1,9 @@
 import { User } from "../models/User";
+import { consumeMessages } from "../utils/user-consumer";
 
 export const getUsers = async () => {
   try {
+    consumeMessages();
     return await User.find();
   } catch (err) {
     console.log(err);
