fb432bc4b90de4b82b3652003a8abde591b38128
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-12-19 19:34:00 -0300

    refactor update user service

--- User-Service/src/services/updateUser.ts
+++ User-Service/src/services/updateUser.ts
@@ -1,12 +1,12 @@
 import { User } from "../models/User";
 import bcrypt from 'bcrypt';
 
-export const updateUser = async (id: String, user: { username: string; password: string; role: string; }) => {
-    try {
-        const hashedPassword = await bcrypt.hash(user.password, 10);
-        user.password = hashedPassword;
-        return await User.findByIdAndUpdate(id, user, { returnDocument: "after" });
-    } catch (err) {
-        console.log(err);
-    }
+export const updateUser = async (user: { id: string, username: string, password: string, role: string, orders: Array<string> }) => {
+  try {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    return await User.findByIdAndUpdate(user.id, user, { returnDocument: "after" });
+  } catch (err) {
+    console.log(err);
+  }
 }
