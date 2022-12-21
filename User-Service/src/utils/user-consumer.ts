b061e378a020a115add1c0ef949ca96706e50773
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-12-21 15:24:03 -0300

    try/catch to rabbitmq and node args localhost add

--- User-Service/src/utils/user-consumer.ts
+++ User-Service/src/utils/user-consumer.ts
@@ -1,25 +1,29 @@
 import amqp, { Message } from 'amqplib';
 import { addOrder } from '../controllers/user-controller';
 
-const amqpURL = 'amqp://localhost:5672'
+const rabbitmq = process.argv[2] || 'rabbitmq'
+const amqpURL = `amqp://${rabbitmq}:5672`
 
 // CONSUME MESSAGES FROM ORDERS QUEUE
 export async function consumeMessages() {
-  const connection = await amqp.connect(amqpURL);
-  const channel = await connection.createChannel();
-  const queue = 'newOrders';
-  console.log('Consuming messages from RabbitMQ: Orders queue');
+  try {
+    const connection = await amqp.connect(amqpURL);
+    const channel = await connection.createChannel();
+    const queue = 'newOrders';
+    console.log('Consuming messages from RabbitMQ: Orders queue');
 
-  await channel.assertQueue(queue, { durable: true });
-  await channel.consume(queue, (msg) => {
+    await channel.assertQueue(queue, { durable: true });
+    await channel.consume(queue, (msg) => {
 
-    // ADD ORDER ID TO USER DATABASE
-    if (msg) {
-      const orderJson = JSON.parse(msg.content.toString());
-      addOrder(orderJson.user, orderJson._id);
+      // ADD ORDER ID TO USER DATABASE
+      if (msg) {
+        const orderJson = JSON.parse(msg.content.toString());
+        addOrder(orderJson.user, orderJson._id);
 
-      channel.ack(msg);
-      console.log('Message consumed.');
-    }
-  }, { consumerTag: 'User-consumer' });
+        channel.ack(msg);
+        console.log('Message consumed.');
+      }
+    }, { consumerTag: 'User-consumer' });
+  } catch (err) { throw new Error; }
+
 }
